8bb3f95a12d8d2a9dd54f08c435a329ffd68c1a5
Dheeraj <109691973+Dheeraj-4855@users.noreply.github.com>
2023-07-30 11:15:09 +0530

    Update script.js

--- codesoft/landing page/script.js
+++ codesoft/landing page/script.js
@@ -28,55 +28,23 @@ const sectionOneObserver = new IntersectionObserver(function(
   },
   sectionOneOptions);
 
-//   const sectionOneObserver2 = new IntersectionObserver(function(
-//     entries,
-//     sectionOneObserver2
-//   ) {
-//     entries.forEach(entry => {
-//         console.log(entry);
-//         // console.log(entry.target);
-//       if (!entry.isIntersecting) {
-//         // navbar.classList.add("nav-scrolled");
-//         rocket.classList.add("move");
-//       } else {
-//         // navbar.classList.remove("nav-scrolled");
-//         rocket.classList.remove("move");
-//       }
-//     });
-//   },
-//   sectionOneOptions);
-
-
   sectionOneObserver.observe(sectionOne);
-//   sectionOneObserver2.observe(sectionOne);
 
 const input = document.querySelector(".menu-box");
 const search = document.querySelector(".search-box");
 const input2 = document.querySelector(".menu-items");
 console.log(input);
 console.log(input2);
-// input.click(function(){
-//     console.log(input[0].checked);
-// })
+
 $("input").click(function(){
     console.log($(this)[0].checked);
     if($(this)[0].checked){
-        // input.classList.add('add');
-        // navbar.classList.remove("nav-box");
 
-        // input2.classList.add('add');
-
-        // search.classList.add('transparent');
     }
     else{
-        // input.classList.remove('add');
 
-        // input2.classList.remove('add');
-        // navbar.classList.add("nav-box");
-
-        // search.classList.remove('transparent');
 
     }
 });
 
-// input.classList.add('inputs');
+
