04632e35f299c5d7d23781c11f890610ac603e0e
Dheeraj <109691973+Dheeraj-4855@users.noreply.github.com>
2023-07-30 11:12:37 +0530

    Update index.js

--- codesoft/calculator/index.js
+++ codesoft/calculator/index.js
@@ -1,20 +1,9 @@
-// console.log("hello world");
+
 let display= document.getElementById('display');
 
-// let buttons = document.getElementsByClassName('button');
-// console.log(buttons);
 
 let buttons = Array.from(document.getElementsByClassName('button'));
-// console.log(buttons);
 
-// buttons.map(button =>{
-//     button.addEventListener('click',(e) =>{
-// console.log('clicked');
-// console.log(e);
-// console.log(e.target);
-// console.log(e.target.innerText);
-//     });
-// });
 
 buttons.map(button => {
     button.addEventListener('click', (e) => {
